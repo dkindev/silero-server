584a99b4d036727f56718be2f6d8e651098dca07
dkindev <omigoneo@gmail.com>
2026-06-21 18:07:34 +0000

    Refactor wayming handler

--- src/handler.py
+++ src/handler.py
@@ -1,6 +1,6 @@
-import logging
 import re
 
+from loguru import logger
 from wyoming.audio import AudioChunk, AudioStart, AudioStop
 from wyoming.error import Error
 from wyoming.event import Event
@@ -14,9 +14,8 @@ from wyoming.tts import (
     SynthesizeStopped,
 )
 
-from src.tts.engine import BYTES_PER_SAMPLE, CHANNELS, SileroTTSEngine
-
-_LOGGER = logging.getLogger(__name__)
+from src import __metadata__, __project_urls__
+from src.tts.engine import SileroTTSEngine
 
 
 def _get_language_from_locale_string(locale_str: str) -> str | None:
@@ -26,7 +25,7 @@ def _get_language_from_locale_string(locale_str: str) -> str | None:
     return parts[0] if parts else None
 
 
-def _create_wyoming_info(engine: SileroTTSEngine) -> Info:
+def _create_wyoming_info(engine: SileroTTSEngine, streaming: bool) -> Info:
     storage = engine.get_storage()
     voices = []
     for voice in storage.get_voices():
@@ -40,6 +39,7 @@ def _create_wyoming_info(engine: SileroTTSEngine) -> Info:
                 ),
                 installed=True,
                 languages=[_get_language_from_locale_string(voice.locale)],
+                version=None,
             )
         )
 
@@ -48,14 +48,16 @@ def _create_wyoming_info(engine: SileroTTSEngine) -> Info:
     return Info(
         tts=[
             TtsProgram(
-                name="silero",
-                description="A fast, local, neural text to speech engine",
+                name=__metadata__["name"],
+                description=__metadata__["summary"],
                 attribution=Attribution(
-                    name="silero",
-                    url="https://github.com/snakers4/silero-models",
+                    name=__metadata__["name"],
+                    url=__project_urls__["Homepage"],
                 ),
                 installed=True,
                 voices=voices,
+                supports_synthesize_streaming=streaming,
+                version=__metadata__["version"],
             )
         ],
     )
@@ -65,64 +67,96 @@ class SileroWyomingHandler(AsyncEventHandler):
     def __init__(
         self,
         engine: SileroTTSEngine,
+        supports_synthesize_streaming: bool,
         *args,
         **kwargs,
     ) -> None:
         super().__init__(*args, **kwargs)
         self.engine = engine
+        self.supports_synthesize_streaming = supports_synthesize_streaming
         self.is_streaming: bool | None = None
         self._synthesize: Synthesize | None = None
 
     async def handle_event(self, event: Event) -> bool:
-        if Describe.is_type(event.type):
-            wyoming_info_event = _create_wyoming_info(self.engine).event()
-            await self.write_event(wyoming_info_event)
-            _LOGGER.debug("Sent info")
-            return True
-
         try:
+            logger.info("Handle '{event_type}'", event_type=event.type)
+            logger.debug("Event: {event}", event=event)
+
+            if Describe.is_type(event.type):
+                return await self._handle_describe(event)
+
             if Synthesize.is_type(event.type):
                 if self.is_streaming:
+                    # Ignore since this is only sent for compatibility reasons.
+                    # For streaming, we expect:
+                    # [synthesize-start] -> [synthesize-chunk]+ -> [synthesize]? -> [synthesize-stop]
                     return True
 
-                synthesize = Synthesize.from_event(event)
-                self._synthesize = Synthesize(text="", voice=synthesize.voice)
-                await self._handle_synthesize(self._synthesize)
+                # Sent outside a stream, so we must process it
+                return await self._handle_synthesize(event)
+
+            if not self.supports_synthesize_streaming:
+                # Streaming is not enabled
                 return True
 
             if SynthesizeStart.is_type(event.type):
-                stream_start = SynthesizeStart.from_event(event)
-                self.is_streaming = True
-                self._synthesize = Synthesize(text="", voice=stream_start.voice)
-                _LOGGER.debug("Text stream started: voice=%s", stream_start.voice)
-                return True
+                return await self._handle_synthesize_start(event)
 
             if SynthesizeChunk.is_type(event.type):
-                assert self._synthesize is not None
-                stream_chunk = SynthesizeChunk.from_event(event)
-                self._synthesize.text += stream_chunk.text
-                return True
+                return await self._handle_synthesize_chunk(event)
 
             if SynthesizeStop.is_type(event.type):
-                assert self._synthesize is not None
-                await self._handle_synthesize(self._synthesize)
-                await self.write_event(SynthesizeStopped().event())
-                _LOGGER.debug("Text stream stopped")
-                return True
-
-            return True
+                return await self._handle_synthesize_stop(event)
         except Exception as err:
             await self.write_event(Error(text=str(err), code=err.__class__.__name__).event())
             raise
 
-    async def _handle_synthesize(self, synthesize: Synthesize) -> bool:
-        _LOGGER.debug(synthesize)
+        return True
+
+    async def _handle_synthesize(self, event: Event) -> bool:
+        synthesize = Synthesize.from_event(event)
+        state = await self._synthesize_pcm_chunks(synthesize=synthesize)
+        return state
+
+    async def _handle_synthesize_start(self, event: Event) -> bool:
+        stream_start = SynthesizeStart.from_event(event)
+        self.is_streaming = True
+        self._synthesize = Synthesize(text="", voice=stream_start.voice)
+        logger.debug("Text stream started: voice='{voice}'", voice=stream_start.voice)
+        return True
+
+    async def _handle_synthesize_chunk(self, event: Event) -> bool:
+        assert self._synthesize is not None
+        stream_chunk = SynthesizeChunk.from_event(event)
+        self._synthesize.text = stream_chunk.text
+        state = await self._synthesize_pcm_chunks(self._synthesize)
+        logger.debug("Text stream processed.")
+        return state
+
+    async def _handle_synthesize_stop(self, event: Event) -> bool:
+        assert self._synthesize is not None
+        await self.write_event(SynthesizeStopped().event())
+        logger.debug("Text stream stopped")
+        return True
+
+    async def _handle_describe(self, event: Event) -> bool:
+        wyoming_info_event = _create_wyoming_info(
+            self.engine, self.supports_synthesize_streaming
+        ).event()
+        await self.write_event(wyoming_info_event)
+        return True
+
+    async def _synthesize_pcm_chunks(self, synthesize: Synthesize) -> bool:
+        logger.debug(synthesize)
 
         text = " ".join(synthesize.text.strip().splitlines())
 
         voice_id = synthesize.voice.name if synthesize.voice else None
         if voice_id is None:
             raise ValueError("No voice specified in Synthesize event")
+
+        if self.engine.get_storage().get_voice(voice_id) is None:
+            raise ValueError("No supported voice")
 
         send_start = True
         async for result in self.engine.synthesize_pcm_chunks(text, voice_id, "TEXT"):
@@ -145,15 +179,6 @@ class SileroWyomingHandler(AsyncEventHandler):
                 ).event()
             )
 
-        if send_start:
-            await self.write_event(
-                AudioStart(
-                    rate=0,
-                    width=BYTES_PER_SAMPLE,
-                    channels=CHANNELS,
-                ).event()
-            )
-
         await self.write_event(AudioStop().event())
-        _LOGGER.info("Synthesis complete")
+
         return True
